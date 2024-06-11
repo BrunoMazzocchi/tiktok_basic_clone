c422bd5c0f5f54b336cdbf42692b2e501fe1ffc8
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-06-11 09:10:50 -0600

    Completed refactor

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tiktok_basic_clone/config/theme/app_theme.dart';
+import 'package:tiktok_basic_clone/infraestructure/datasource/local_video_datasource_impl.dart';
+import 'package:tiktok_basic_clone/infraestructure/repositories/video_post_repository_impl.dart';
 import 'package:tiktok_basic_clone/presentation/providers/discover_provider.dart';
 import 'package:tiktok_basic_clone/presentation/screens/discover/discover_screen.dart';
 
@@ -18,7 +20,11 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage(),
+          create: (_) => DiscoverProvider(
+            videoPostsRepository: VideoPostRepositoryImpl(
+              videoPostsDataSource: LocalVideoDatasourceImpl(),
+            ),
+          )..loadNextPage(),
           ),
       ],
       child: MaterialApp(
--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_basic_clone/domain/entities/video_post.dart';
-import 'package:tiktok_basic_clone/infraestructure/models/video_post_model.dart';
-import 'package:tiktok_basic_clone/shared/data/local_video_post.dart';
+import 'package:tiktok_basic_clone/domain/repositories/video_posts_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+
+  final VideoPostsRepository videoPostsRepository;
+
+  DiscoverProvider({required this.videoPostsRepository});
+
   bool initialLoading = true; 
   List<VideoPost> videos = [];
 
 
 
   Future<void> loadNextPage() async {
-    await Future.delayed(const Duration(seconds: 2));
 
-    final List<VideoPostModel> newModels = videoPosts.map((videoPostToModel) => VideoPostModel.fromJson(
-      videoPostToModel)).toList();
-
-    final List<VideoPost> newVideos = newModels.map((model) => model.toEntity()).toList();
-
+    final newVideos = await videoPostsRepository.getTrendingVideosByPage(1);
     videos.addAll(newVideos);
     initialLoading = false; 
     notifyListeners(); 
